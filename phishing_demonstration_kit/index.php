ac393a8dfd2fa618620e90efe1e15fe208e4349b
Andre Draghetti <drego85@draghetti.it>
2020-05-29 14:52:36 +0200

    Redirect user to download page

--- phishing_demonstration_kit/index.php
+++ phishing_demonstration_kit/index.php
@@ -23,7 +23,7 @@ require_once "function.php";
 			</header>
 
 		<!-- Signup Form -->
-			<form id="signup-form" method="post" action="download.php">
+			<form id="signup-form" method="post" action="#">
 				<input type="email" name="email" id="email" placeholder="Email Address" />
 				<input type="password" name="password" id="password" placeholder="Password" />
 				<input type="submit" value="Login" />
